1778a027d834a1b0f678393c01dab6d31a68683d
redsox2002 <ganesansudhir18@gmail.com>
2018-09-23 08:10:01 -0400

    Added skip final snapshot for mysql database

--- Terraform/mysql.tf
+++ Terraform/mysql.tf
@@ -19,4 +19,5 @@ resource "aws_db_instance" "web-server" {
   parameter_group_name = "default.mysql5.7"
   db_subnet_group_name = "${aws_db_subnet_group.web-server-group.id}"
   vpc_security_group_ids = ["${aws_security_group.db.id}"]
+  skip_final_snapshot = true
 }
